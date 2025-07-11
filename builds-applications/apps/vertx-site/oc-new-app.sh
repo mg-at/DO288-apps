9a8277f037535872b6393af80e2a0a0653a65276
mg-at <165230852+mg-at@users.noreply.github.com>
2025-07-11 16:12:46 +1200

    Update oc-new-app.sh

--- builds-applications/apps/vertx-site/oc-new-app.sh
+++ builds-applications/apps/vertx-site/oc-new-app.sh
@@ -4,4 +4,4 @@ oc new-app --name vertx-site \
   --build-env MAVEN_MIRROR_URL=http://nexus-infra.apps.ocp4.example.com/repository/java \
   -i redhat-openjdk18-openshift:1.8 \
   --context-dir builds-applications/apps/vertx-site \
-  https://git.ocp4.example.com/developer/DO288-apps
+  https://github.com/mg-at/DO288-apps
